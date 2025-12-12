7c316daab40eba259c1ee7f8a0373bc9826d4cb4
Bump Dolwijit <42529403+dolwijit13@users.noreply.github.com>
2025-12-12 17:25:48 +0900

    Add property model (#4)
    * Add pry-rails gem
    
    * Add migration and update schema
    
    * Initialize Property model with property_type enum
    
    * Add validation
    
    * Fix data types for unique_id and size in property migration
    
    * Add property factory bot
    
    * Add property model spec
    
    * Add frozen_string_literal comment
    
    * Add unit tests for property_type_in_japanese

---
+++ app/models/property.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Property < ApplicationRecord
+  module PropertyTypes
+    APARTMENT = 'アパート'
+    HOUSE = '一戸建て'
+    CONDO = 'マンション'
+  end
+
+  enum property_type: {
+    apartment: PropertyTypes::APARTMENT,
+    house: PropertyTypes::HOUSE,
+    condo: PropertyTypes::CONDO
+  }
+
+  validates :unique_id, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :room_number, presence: true, unless: :house?
+
+  def property_type_in_japanese
+    self.class.property_types[self[:property_type]]
+  end
+end
---
+++ spec/factories/property.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :property do
+    sequence(:unique_id)
+    name { 'property_name' }
+    property_type { %i[house apartment condo].sample }
+    room_number { property_type == 'house' ? nil : (101..999).to_a.sample }
+    address { 'address' }
+    rent { (50_000..150_000).to_a.sample }
+    size { (20..150).to_a.sample }
+  end
+end
