8efa4e0ce36af275c07b138c218cffb12b4bd17a
Bump Dolwijit <42529403+dolwijit13@users.noreply.github.com>
2025-12-15 18:11:01 +0900

    Add an API to import properties (#6)
    * Fix property import parse row incorrectly
    
    * Add api/properties#import API
    
    * Add the API spec
    
    * Change the status code to 201 created
    
    * Refactor coding style
    
    * Remove debugger
    
    * Update describe text to be API path

---
+++ app/controllers/api/properties_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Api
+  class PropertiesController < ApplicationController
+    def import
+      PropertyImporter.new(params[:file].path).call
+      render json: { message: 'Import successful' }, status: :created
+    end
+  end
+end
