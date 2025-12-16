1bdef20989920839011c8155871d0cbe0f2373f9
dolwijit.j <dolwijit.j@gmail.com>
2025-12-16 23:49:21 +0900

    Add pagination and total count to UI

--- app/javascript/components/PropertyList.jsx
+++ app/javascript/components/PropertyList.jsx
@@ -3,7 +3,7 @@ import Property from "./Property"
 import UploadButton from "./UploadButton"
 
 const PropertyList = (props) => {
-  const { properties } = props
+  const { properties, total_count } = props
 
   const headers = [
     "ユニークID",
@@ -16,38 +16,40 @@ const PropertyList = (props) => {
     "作成日時"
   ]
 
-  if (!properties || properties.length === 0) {
-    return (
-      <div className="property-list-container">
-        <h1 className="property-list-title">Property List</h1>
-        <div className="property-list-empty">
-          <h3>No Properties Found</h3>
-          <p>There are currently no properties to display. Import some properties to get started.</p>
-        </div>
-      </div>
-    )
-  }
+  const emptySection = (
+    <div className="property-list-empty">
+      <h3>No Properties Found</h3>
+      <p>There are currently no properties to display. Import some properties to get started.</p>
+    </div>
+  )
+
+  const tableSection = (
+    <table className="property-table">
+      <thead className="property-table-header">
+        <tr>
+          {headers.map((header) => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {properties.map((property) => (
+          <Property key={property.id} {...property} />
+        ))}
+      </tbody>
+    </table>
+  )
 
   return (
     <div className="property-list-container">
       <div className="property-list-header">
-        <h1 className="property-list-title">Property List</h1>
+        <div className="property-list-info">
+          <h1 className="property-list-title">Property List</h1>
+          <span className="property-list-count">{total_count} properties found</span>
+        </div>
         <UploadButton />
       </div>
-      <table className="property-table">
-        <thead className="property-table-header">
-          <tr>
-            {headers.map((header) => (
-              <th key={header}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {properties.map((property) => (
-            <Property key={property.id} {...property} />
-          ))}
-        </tbody>
-      </table>
+      {properties.length === 0 ? emptySection : tableSection}
     </div>
   )
 }
